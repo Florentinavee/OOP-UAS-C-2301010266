7eabb185513352a7816fd7af3479b176aba4aafb
Florentinavee <flominovela@gmail.com>
2025-06-28 14:52:09 +0800

    Add files via upload

---
+++ OOP_UAS_C_2301010266/src/oop_uas_c_2301010266/OOP_UAS_C_2301010266.java
@@ -0,0 +1,17 @@
+package oop_uas_c_2301010266;
+
+/**
+ *
+ * @author Florentina Minovela Nasa
+ * Tanggal : 28 Juni 2025
+ */
+public class OOP_UAS_C_2301010266 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+      
+    }
+    
+}
